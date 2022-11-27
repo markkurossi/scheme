bb5b6a206ef251435afa9397eae20d78f59bda1f
Markku Rossi <mtr@iki.fi>
2022-11-27 16:33:51 +0100

    Command history.

--- cmd/scheme/main.go
+++ cmd/scheme/main.go
@@ -9,13 +9,14 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/markkurossi/scheme"
 	"github.com/peterh/liner"
 )
 
 func main() {
-	fmt.Printf("Hello, Scheme!\n")
+	fmt.Printf("Go Scheme Version 0.0\n")
 
 	scm, err := scheme.New()
 	if err != nil {
@@ -30,6 +31,7 @@ func main() {
 	defer input.liner.Close()
 
 	for {
+		input.count = 0
 		v, err := scm.Eval("input", input)
 		if err != nil {
 			fmt.Printf("scheme.Eval: %v\n", err)
@@ -40,13 +42,16 @@ func main() {
 			break
 		}
 		fmt.Printf(" => %v\n", v)
+		input.SaveHistory()
 	}
 }
 
 type input struct {
 	scm   *scheme.Scheme
+	count int
 	liner *liner.State
 	buf   []byte
+	line  []byte
 }
 
 func (i *input) Read(p []byte) (n int, err error) {
@@ -55,15 +60,31 @@ func (i *input) Read(p []byte) (n int, err error) {
 			return 0, io.EOF
 		}
 
+		prompt := "scm > "
+		if i.count > 0 {
+			prompt = "    >   "
+		}
+
 		var line string
-		line, err = i.liner.Prompt("scm > ")
+		line, err = i.liner.Prompt(prompt)
 		if err != nil {
 			return
 		}
 		i.buf = []byte(line)
-		i.buf = append(i.buf, '\n')
+		i.buf = append(i.buf, ' ')
+		i.count++
 	}
 	n = copy(p, i.buf)
+	i.line = append(i.line, i.buf[:n]...)
+
 	i.buf = i.buf[n:]
+
 	return
+}
+
+func (i *input) SaveHistory() {
+	if len(i.line) > 0 {
+		i.liner.AppendHistory(strings.TrimSpace(string(i.line)))
+	}
+	i.line = nil
 }
