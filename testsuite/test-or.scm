a78391d016d46f20e9a8aaea6fd0f28fc347793c
Markku Rossi <mtr@iki.fi>
2023-01-09 06:58:07 +0100

    Refactoring test files.

--- testsuite/test-or.scm
+++ testsuite/test-or.scm
@@ -4,21 +4,6 @@
 ;;; All rights reserved.
 ;;;
 
-(runner 'run "and"
-        (lambda (t)
-          (if (not (eq? (and) #t))
-              (t 'error "(and)")))
-        (lambda (t)
-          (if (not (eq? (and (= 2 2) (> 2 1)) #t))
-              (t 'error "(and #t)")))
-        (lambda (t)
-          (if (not (eq? (and (= 2 2) (< 2 1)) #f))
-              (t 'error "(and #f)")))
-        (lambda (t)
-          (if (not (equal? (and 1 2 'c '(f g)) '(f g)))
-              (t 'error "(and '(f g))")))
-        )
-
 (runner 'run "or"
         (lambda (t)
           (if (not (eq? (or) #f))
