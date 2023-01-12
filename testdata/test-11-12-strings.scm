0f0d0dbf74331142b0d621e1887f6be1fc7f7023
Markku Rossi <mtr@iki.fi>
2023-01-12 12:51:50 +0100

    Cleaned up.

--- testdata/test-11-12-strings.scm
+++ testdata/test-11-12-strings.scm
@@ -6,16 +6,17 @@
 
 (runner 'sub-section "11.12. Strings")
 
-(runner 'run "strings"
-        (lambda (t)
-          (letrec ((iter
+(runner 'test "strings"
+        (lambda ()
+          (letrec ((errors 0)
+                   (iter
                     (lambda (values)
                       (if (null? values)
                           #t
                           (begin
                             (if (not (equal? (string->list (caar values))
                                              (cadar values)))
-                                (t 'error "string mismatch"))
+                                (set! errors (+ errors 1)))
                             (iter (cdr values)))))))
             (iter '(("abc" (#\x61 #\x62 #\x63))
                     ("\x41;bc" (#\x41 #\x62 #\x63))
@@ -24,154 +25,93 @@
                     ("\x00000041;" (#\x41))
                     ("\x0010FFFF;" (#\x10FFFF))
                     ("\x000000001;" (#\x1))
-                    )))))
+                    ))
+            (= errors 0)
+            )))
 
-(runner 'run "string?"
-        (lambda (t)
-          (if (not (string? ""))
-              (t 'error "char? \"\"")))
-        (lambda (t)
-          (if (string? #\a)
-              (t 'error "char? #\\a"))))
-(runner 'run "make-string"
-        (lambda (t)
-          (if (not (string=? (make-string 5) "     "))
-              (t 'error "make-string 5")))
-        (lambda (t)
-          (if (not (string=? (make-string 5 #\@) "@@@@@"))
-              (t 'error "make-string @")))
-        (lambda (t)
-          (if (not (string=? (make-string 2 #\x41bc) "\x41bc;\x41bc;"))
-              (t 'error "make-string u41bc"))))
-(runner 'run "string"
-        (lambda (t)
-          (if (not (string=? (string #\a #\b #\  #\c) "ab c"))
-              (t 'error "string"))))
-(runner 'run "string-length"
-        (lambda (t)
-          (if (not (= (string-length "") 0))
-              (t 'error "string-length 0")))
-        (lambda (t)
-          (if (not (= (string-length "foo") 3))
-              (t 'error "string-length 3")))
-        (lambda (t)
-          (if (not (= (string-length "\x41bc;") 1))
-              (t 'error "string-length \\x41bc"))))
-(runner 'run "string-ref"
-        (lambda (t)
-          (if (not (char=? (string-ref ">\x41bc;<" 0) #\>))
-              (t 'error "string-ref 1")))
-        (lambda (t)
-          (if (not (char=? (string-ref ">\x41bc;<" 1) #\x41bc))
-              (t 'error "string-ref 1")))
-        (lambda (t)
-          (if (not (char=? (string-ref ">\x41bc;<" 2) #\<))
-              (t 'error "string-ref 1"))))
+(runner 'test "string?"
+        (lambda () (string? ""))
+        (lambda () (not (string? #\a)))
+        )
+(runner 'test "make-string"
+        (lambda () (string=? (make-string 5) "     "))
+        (lambda () (string=? (make-string 5 #\@) "@@@@@"))
+        (lambda () (string=? (make-string 2 #\x41bc) "\x41bc;\x41bc;"))
+        )
 
-(runner 'run "string=?"
-        (lambda (t)
-          (if (not (string=? "" ""))
-              (t 'error "string=? empty")))
-        (lambda (t)
-          (if (not (string=? "" "" ""))
-              (t 'error "string=? empty")))
-        (lambda (t)
-          (if (not (string=? "abc" "abc" "abc"))
-              (t 'error "string=? abc")))
-        (lambda (t)
-          (if (string=? "abc" "abc" "abcd")
-              (t 'error "string=? abc abcd"))))
+(runner 'test "string"
+        (lambda () (string=? (string #\a #\b #\  #\c) "ab c"))
+        )
 
-(runner 'run "string<?"
-        (lambda (t)
-          (if (not (string<? "a" "b"))
-              (t 'error "string<? a b")))
-        (lambda (t)
-          (if (not (string<? "a" "b" "c"))
-              (t 'error "string<? a b c")))
-        (lambda (t)
-          (if (string<? "a" "b" "b")
-              (t 'error "string<? a b b"))))
-(runner 'run "string>?"
-        (lambda (t)
-          (if (not (string>? "b" "a"))
-              (t 'error "string>? b a")))
-        (lambda (t)
-          (if (not (string>? "c" "b" "a"))
-              (t 'error "string>? c b a")))
-        (lambda (t)
-          (if (string>? "c" "b" "b")
-              (t 'error "string>? c b b"))))
-(runner 'run "string<=?"
-        (lambda (t)
-          (if (not (string<=? "a" "b"))
-              (t 'error "string<=? a b")))
-        (lambda (t)
-          (if (not (string<=? "a" "b" "c"))
-              (t 'error "string<=? a b c")))
-        (lambda (t)
-          (if (not (string<=? "a" "b" "b"))
-              (t 'error "string<=? a b b")))
-        (lambda (t)
-          (if (string<=? "a" "b" "a")
-              (t 'error "string<=? a b a"))))
-(runner 'run "string=>?"
-        (lambda (t)
-          (if (not (string>=? "b" "a"))
-              (t 'error "string>=? b a")))
-        (lambda (t)
-          (if (not (string>=? "c" "b" "a"))
-              (t 'error "string>=? c b a")))
-        (lambda (t)
-          (if (not (string>=? "c" "b" "b"))
-              (t 'error "string>=? c b b")))
-        (lambda (t)
-          (if (string>=? "c" "b" "c")
-              (t 'error "string>=? c b c"))))
+(runner 'test "string-length"
+        (lambda () (= (string-length "") 0))
+        (lambda () (= (string-length "foo") 3))
+        (lambda () (= (string-length "\x41bc;") 1))
+        )
 
-(runner 'run "substring"
-        (lambda (t)
-          (if (not (string=? (substring "foo" 0 0) ""))
-              (t 'error "substring 0 0")))
-        (lambda (t)
-          (if (not (string=? (substring "foo" 0 1) "f"))
-              (t 'error "substring 0 1")))
-        (lambda (t)
-          (if (not (string=? (substring "foo" 1 2) "o"))
-              (t 'error "substring 1 2")))
-        (lambda (t)
-          (if (not (string=? (substring "foo" 1 3) "oo"))
-              (t 'error "substring 1 3"))))
+(runner 'test "string-ref"
+        (lambda () (char=? (string-ref ">\x41bc;<" 0) #\>))
+        (lambda () (char=? (string-ref ">\x41bc;<" 1) #\x41bc))
+        (lambda () (char=? (string-ref ">\x41bc;<" 2) #\<))
+        )
 
-(runner 'run "string-append"
-        (lambda (t)
-          (if (not (string=? (string-append) ""))
-              (t 'error "string-append")))
-        (lambda (t)
-          (if (not (string=? (string-append ">" "\x41bc;" "<") ">\x41bc;<"))
-              (t 'error "string-append"))))
+(runner 'test "string=?"
+        (lambda () (string=? "" ""))
+        (lambda () (string=? "" "" ""))
+        (lambda () (string=? "abc" "abc" "abc"))
+        (lambda () (not (string=? "abc" "abc" "abcd")))
+        )
 
-(runner 'run "string->list"
-        (lambda (t)
-          (if (not (equal? (string->list "a\x41bc;c")
-                           '(#\a #\x41bc #\c)))
-              (t 'error "string->list")))
-        (lambda (t)
-          (if (not (equal? (string->list "") '()))
-              (t 'error "string->append"))))
-(runner 'run "list->string"
-        (lambda (t)
-          (if (not (string=? (list->string (string->list "a\x41bc;c"))
+(runner 'test "string<?"
+        (lambda () (string<? "a" "b"))
+        (lambda () (string<? "a" "b" "c"))
+        (lambda () (not (string<? "a" "b" "b")))
+        )
+
+(runner 'test "string>?"
+        (lambda () (string>? "b" "a"))
+        (lambda () (string>? "c" "b" "a"))
+        (lambda () (not (string>? "c" "b" "b")))
+        )
+
+(runner 'test "string<=?"
+        (lambda () (string<=? "a" "b"))
+        (lambda () (string<=? "a" "b" "c"))
+        (lambda () (string<=? "a" "b" "b"))
+        (lambda () (not (string<=? "a" "b" "a")))
+        )
+
+(runner 'test "string=>?"
+        (lambda () (string>=? "b" "a"))
+        (lambda () (string>=? "c" "b" "a"))
+        (lambda () (string>=? "c" "b" "b"))
+        (lambda () (not (string>=? "c" "b" "c")))
+        )
+
+(runner 'test "substring"
+        (lambda () (string=? (substring "foo" 0 0) ""))
+        (lambda () (string=? (substring "foo" 0 1) "f"))
+        (lambda () (string=? (substring "foo" 1 2) "o"))
+        (lambda () (not (not (string=? (substring "foo" 1 3) "oo"))))
+        )
+
+(runner 'test "string-append"
+        (lambda () (string=? (string-append) ""))
+        (lambda () (string=? (string-append ">" "\x41bc;" "<") ">\x41bc;<"))
+        )
+
+(runner 'test "string->list"
+        (lambda () (equal? (string->list "a\x41bc;c") '(#\a #\x41bc #\c)))
+        (lambda () (equal? (string->list "") '()))
+        )
+
+(runner 'test "list->string"
+        (lambda () (string=? (list->string (string->list "a\x41bc;c"))
                              "a\x41bc;c"))
-              (t 'error "list->string")))
-        (lambda (t)
-          (if (not (string=? (list->string (string->list "")) ""))
-              (t 'error "string->append"))))
+        (lambda () (string=? (list->string (string->list "")) ""))
+        )
 
-(runner 'run "string-copy"
-        (lambda (t)
-          (if (not (string=? (string-copy "") ""))
-              (t 'error "string-copy")))
-        (lambda (t)
-          (if (not (string=? (string-copy "a\x41bc;c") "a\x41bc;c"))
-              (t 'error "string-copy"))))
+(runner 'test "string-copy"
+        (lambda () (string=? (string-copy "") ""))
+        (lambda () (string=? (string-copy "a\x41bc;c") "a\x41bc;c"))
+        )
