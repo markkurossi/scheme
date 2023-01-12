27fa540d29fdef45adde7b91563d0a894173e4e8
Markku Rossi <mtr@iki.fi>
2023-01-12 07:11:10 +0100

    Refactored to use 'test instead of 'run.

--- testdata/test-11-07-arithmetic.scm
+++ testdata/test-11-07-arithmetic.scm
@@ -6,23 +6,13 @@
 
 (runner 'sub-section "11.7. Arithmetic")
 
-(runner 'run "odd?"
-        (lambda (t)
-          (if (not (odd? 1))
-              (t 'error "odd? 1")))
-        (lambda (t)
-          (if (not (odd? #e1))
-              (t 'error "odd? #e1")))
+(runner 'test "odd?"
+        (lambda ()(odd? 1))
+        (lambda ()(odd? #e1))
         )
 
-(runner 'run "even?"
-        (lambda (t)
-          (if (not (even? 0))
-              (t 'error "even? 0")))
-        (lambda (t)
-          (if (not (even? 42))
-              (t 'error "even? 42")))
-        (lambda (t)
-          (if (not (even? #e42))
-              (t 'error "even? #e42")))
+(runner 'test "even?"
+        (lambda () (even? 0))
+        (lambda () (even? 42))
+        (lambda () (even? #e42))
         )
--- testdata/test-11-10-symbols.scm
+++ testdata/test-11-10-symbols.scm
@@ -6,70 +6,38 @@
 
 (runner 'sub-section "11.10. Symbols")
 
-(runner 'run "symbol?"
-        (lambda (t)
-          (if (not (symbol? 'foo))
-              (t 'error "symbol? 'foo")))
-        (lambda (t)
-          (if (not (symbol? (car '(a b))))
-              (t 'error "symbol? (car '(a b))")))
-        (lambda (t)
-          (if (symbol? "bar")
-              (t 'error "symbol? \"bar\"")))
-        (lambda (t)
-          (if (not (symbol? 'nil))
-              (t 'error "symbol? 'nil")))
-        (lambda (t)
-          (if (symbol? '())
-              (t 'error "symbol? '()")))
-        (lambda (t)
-          (if (symbol? #f)
-              (t 'error "symbol? #f")))
+(runner 'test "symbol?"
+        (lambda () (symbol? 'foo))
+        (lambda () (symbol? (car '(a b))))
+        (lambda () (not (symbol? "bar")))
+        (lambda () (symbol? 'nil))
+        (lambda () (not (symbol? '())))
+        (lambda () (not (symbol? #f)))
         )
 
-(runner 'run "symbol->string"
-        (lambda (t)
-          (if (not (string=? (symbol->string 'flying-fish)
+(runner 'test "symbol->string"
+        (lambda () (string=? (symbol->string 'flying-fish)
                              "flying-fish"))
-              (t 'error "symbol->string 'flying-fish")))
-        (lambda (t)
-          (if (not (string=? (symbol->string 'Martin)
+        (lambda () (string=? (symbol->string 'Martin)
                              "Martin"))
-              (t 'error "symbol->string \"Martin\"")))
-        (lambda (t)
-          (if (not (string=? (symbol->string (string->symbol "Malvina"))
+        (lambda () (string=? (symbol->string (string->symbol "Malvina"))
                              "Malvina"))
-              (t 'error "string->symbol->string \"Malvina\"")))
         )
 
-(runner 'run "symbol=?"
-        (lambda (t)
-          (if (not (symbol=? 'a 'a))
-              (t 'error "symbol=? 'a 'a")))
-        (lambda (t)
-          (if (not (symbol=? 'a 'a 'a))
-              (t 'error "symbol=? 'a 'a 'a")))
+(runner 'test "symbol=?"
+        (lambda () (symbol=? 'a 'a))
+        (lambda () (symbol=? 'a 'a 'a))
         )
 
-(runner 'run "string->symbol"
-        (lambda (t)
-          (if (eq? 'mISSISSIppi 'mississippi)
-              (t 'error "eq? 'mISSISSIppi 'mississippi")))
-        (lambda (t)
-          (if (not (symbol=? (string->symbol "mISSISSIppi")
+(runner 'test "string->symbol"
+        (lambda () (not (eq? 'mISSISSIppi 'mississippi)))
+        (lambda () (symbol=? (string->symbol "mISSISSIppi")
                              'mISSISSIppi))
-              (t 'error "string->symbol \"mISSISSIppi\"")))
-        (lambda (t)
-          (if (not (eq? 'bitBlt (string->symbol "bitBlt")))
-              (t 'error "eq? 'bitBlt")))
-        (lambda (t)
-          (if (not (eq? 'JollyWog
+        (lambda () (eq? 'bitBlt (string->symbol "bitBlt")))
+        (lambda () (eq? 'JollyWog
                         (string->symbol
                          (symbol->string 'JollyWog))))
-              (t 'error "eq? symbol->string->symbol 'JollyWog")))
-        (lambda (t)
-          (if (not (string=? "K. Harper, M.D."
+        (lambda () (string=? "K. Harper, M.D."
                              (symbol->string
                               (string->symbol "K. Harper, M.D."))))
-              (t 'error "string=? string->symbol->string \"K. Harper, M.D.\"")))
         )
