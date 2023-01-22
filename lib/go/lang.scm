5aa0f969f5bb453c926c1ddc69b98e1f1910a3c4
Markku Rossi <mtr@iki.fi>
2023-01-22 12:07:27 +0100

    Library for Go language support.

---
+++ lib/go/lang.scm
@@ -0,0 +1,21 @@
+;;;
+;;; Copyright (c) 2023 Markku Rossi
+;;;
+;;; All rights reserved.
+;;;
+
+(library (go lang)
+  (export len)
+  (import (rnrs))
+
+  ;; (display "This is initializer for the (go lang) library.") (newline)
+
+  (define (len obj)
+    (cond
+     ((string? obj) (string-length obj))
+     ((bytevector? obj) (bytevector-length obj))
+     ((list? obj) (length obj))
+     ((vector? obj) (vector-length obj))
+     (else
+      (error 'len "invalid object" obj))))
+  )
