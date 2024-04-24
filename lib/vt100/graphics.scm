d63e4d880d4772b790943045ceee537d46495346
Markku Rossi <mtr@iki.fi>
2024-04-24 08:51:35 +0100

    ASCII graphics.

---
+++ lib/vt100/graphics.scm
@@ -0,0 +1,31 @@
+;;;
+;;; Copyright (c) 2023 Markku Rossi
+;;;
+;;; All rights reserved.
+;;;
+
+(library (vt100 graphics (1 0))
+  (export hblock)
+  (import (vt100 base) (rnrs base))
+
+  (define (hblock width fract)
+    (if (< fract 0.0)
+        (set! fract 0.0))
+    (if (> fract 1.0)
+        (set! fract 1.0))
+    (letrec ((w8 (integer->float (* width 8)))
+             (w (float->integer (* w8 fract)))
+             (wrem (mod w 8))
+             (fill (lambda (str count ch)
+                     (if (zero? count)
+                         str
+                         (fill (string-append str ch) (- count 1) ch))))
+             (result ""))
+      (set! result (fill result (/ w 8) "\x2588;"))
+      (if (> wrem 0)
+          (set! result
+                (string-append result
+                               (list->string
+                                (list (integer->char (- #x2590 wrem)))))))
+      (fill result (- width (string-length result)) " ")))
+  )
