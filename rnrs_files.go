feb380fdfedb687aff56421c9b7a2faffea1e98d
Markku Rossi <mtr@iki.fi>
2025-05-24 09:30:42 +0200

    Added directory-list procedure.

--- rnrs_files.go
+++ rnrs_files.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2023 Markku Rossi
+// Copyright (c) 2023-2025 Markku Rossi
 //
 // All rights reserved.
 //
@@ -44,6 +44,42 @@ var rnrsFilesBuiltins = []Builtin{
 				return nil, err
 			}
 			return Boolean(true), nil
+		},
+	},
+	{
+		Name: "directory-list",
+		Args: []string{"[path<string>]"},
+		Return: &types.Type{
+			Enum: types.EnumPair,
+			Car:  types.String,
+			Cdr:  types.Any,
+		},
+		Native: func(scm *Scheme, args []Value) (Value, error) {
+			dir := "."
+			if len(args) == 1 {
+				d, ok := args[0].(String)
+				if !ok {
+					return nil, fmt.Errorf("invalid path: %v", args[0])
+				}
+				dir = string(d)
+			}
+			f, err := os.Open(dir)
+			if err != nil {
+				return nil, err
+			}
+			defer f.Close()
+
+			names, err := f.Readdirnames(0)
+			if err != nil {
+				return nil, err
+			}
+
+			var result Pair
+			for i := len(names) - 1; i >= 0; i-- {
+				result = NewPair(String(names[i]), result)
+			}
+
+			return result, nil
 		},
 	},
 }
