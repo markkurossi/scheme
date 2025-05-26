f98b5d4c84d313126f57ce968426d814ec6865e7
Markku Rossi <mtr@iki.fi>
2025-05-26 13:14:22 +0200

    SRFI 13: String Libraries.

---
+++ lib/srfi/13.scm
@@ -0,0 +1,31 @@
+;;; -*- compile-command: "scheme vet"; -*-
+;;;
+;;; Copyright (c) 2025 Markku Rossi
+;;;
+;;; All rights reserved.
+;;;
+;;; SRFI 13: String Libraries - https://srfi.schemers.org/srfi-13/srfi-13.html
+;;;
+
+(library (srfi 13 (1 0))
+  (export string-pad string-pad-right)
+  (import (rnrs base))
+
+  (define (string-pad s len . args)
+    (let ((l (string-length s))
+          (ch #\space))
+      (if (not (null? args))
+          (set! ch (car args)))
+      (if (>= l len)
+          (substring s (- l len) l)
+          (string-append (make-string (- len l) ch) s))))
+
+  (define (string-pad-right s len . args)
+    (let ((l (string-length s))
+          (ch #\space))
+      (if (not (null? args))
+          (set! ch (car args)))
+      (if (>= l len)
+          (substring s 0 len)
+          (string-append s (make-string (- len l) ch)))))
+  )
