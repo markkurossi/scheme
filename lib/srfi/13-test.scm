b1c8dba151f074b9cc70492bf9ce06706f759456
Markku Rossi <mtr@iki.fi>
2025-06-01 08:39:48 +0200

    Added string-join tests.

--- lib/srfi/13-test.scm
+++ lib/srfi/13-test.scm
@@ -10,6 +10,24 @@
 (define (test-string-pad t)
   (t 'eq? (string-pad "foobar" 8 #\~) "~~foobar")
   (t 'eq? (string-pad "foobar" 3 #\~) "bar")
+  )
+
+(define (test-string-pad-right t)
   (t 'eq? (string-pad-right "foobar" 8 #\~) "foobar~~")
   (t 'eq? (string-pad-right "foobar" 3 #\~) "foo")
+  )
+
+(define (test-string-join t)
+  (t 'eq? (string-join '("a" "b" "c") "~") "a~b~c")
+  (t 'eq? (string-join '("a") "~") "a")
+  (t 'eq? (string-join '("") "~") "")
+  (t 'eq? (string-join '("a" "b" "c") "~") "a~b~c" 'infix)
+
+  (t 'eq? (string-join '("a" "b" "c") "~" 'prefix) "~a~b~c")
+  (t 'eq? (string-join '("a") "~" 'prefix) "~a")
+  (t 'eq? (string-join '("") "~" 'prefix) "~")
+
+  (t 'eq? (string-join '("a" "b" "c") "~" 'suffix) "a~b~c~")
+  (t 'eq? (string-join '("a") "~" 'suffix) "a~")
+  (t 'eq? (string-join '("") "~" 'suffix) "~")
   )
