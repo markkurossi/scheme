1f9bf93f73df004a35ab1051932c70d7e5b91e54
Markku Rossi <mtr@iki.fi>
2025-06-01 08:40:35 +0200

    SRFI 13 testing.

--- cmd/scheme/test.scm
+++ cmd/scheme/test.scm
@@ -1,18 +1,4 @@
-(import (html unescape))
+(import (srfi 13))
 
-(pragma (verbose-typecheck #f))
-
-(define (test-entities pos)
-  (if (>= pos (vector-length html-entities))
-      #t
-      (let* ((entity (vector-ref html-entities pos))
-             (input (string-append "&" (car entity) " "))
-             (result (html-unescape input)))
-        (if (> (string-length result) 3)
-            (begin
-              (display "entity ") (display pos) (display ": ")
-              (display input) (display "=> ") (display result)
-              (newline)))
-        (test-entities (+ pos 1)))))
-
-(test-entities 0)
+(display (string-join '("a" "b" "c") "~" 'prefix))
+(newline)
