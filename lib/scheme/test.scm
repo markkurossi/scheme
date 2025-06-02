323b632d97f7c650bdeaf3c3fec6897b1ca939fa
Markku Rossi <mtr@iki.fi>
2025-06-02 06:49:49 +0200

    Return test success status.

--- lib/scheme/test.scm
+++ lib/scheme/test.scm
@@ -11,13 +11,14 @@
 
   (define scheme::test::count 0)
   (define scheme::test::errors 0)
+  (define scheme::test::failed #f)
   (define scheme::test::log '())
 
   (define (scheme::test::runner verbose . args)
     (if (null? args)
         (scheme::test::handle-arg ".")
-        (for-each scheme::test::handle-arg args)))
-
+        (for-each scheme::test::handle-arg args))
+    scheme::test::failed)
 
   (define (scheme::test::handle-arg arg)
     (if (string-suffix? "/..." arg)
@@ -74,7 +75,10 @@
                       (scheme::test::run-test export t)
                       ))
                 (caddr lib))
+      ;; (set! scheme::test::count (+ scheme::test::count num-tests))
+
       (cond ((> num-errors 0)
+             (set! scheme::test::failed #t)
              (display "fail "))
             ((> num-tests 0)
              (display "ok   "))
