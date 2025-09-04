63dafa4c3d3376f95cb0070d0d4c0eaa53016eb2
Markku Rossi <mtr@iki.fi>
2025-09-04 11:54:43 +0200

    Added (rnrs (6)) library.

---
+++ lib/rnrs.scm
@@ -0,0 +1,31 @@
+;;;
+;;; Copyright (c) 2025 Markku Rossi
+;;;
+;;; All rights reserved.
+;;;
+
+(library (rnrs (6))
+  (export)
+  (import (rnrs base)
+          (rnrs control)
+          ;; (rnrs conditions)
+          (rnrs exceptions)
+          ;; (rnrs io ports)
+          (rnrs io simple)
+          (rnrs files)
+          (rnrs programs)
+          ;; (rnrs arithmetic bitwise)
+          ;; (rnrs arithmetic fixnums)
+          ;; (rnrs arithmetic flonums)
+          (rnrs bytevectors)
+          (rnrs lists)
+          (rnrs sorting)
+          ;; (rnrs records syntactic)
+          ;; (rnrs records procedural)
+          ;; (rnrs hashtables)
+          ;; (rnrs enums)
+          (rnrs mutable-pairs)
+          (rnrs mutable-strings)
+          ;; (rnrs r5rs)
+          )
+  )
